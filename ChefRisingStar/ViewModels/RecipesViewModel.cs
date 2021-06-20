439cedc036d765b857cfe76a757f39d3fd4cc35f
Keval Pandit <kevalpandit@Ajays-MBP.fios-router.home>
2021-06-20 16:45:52 -0400

    Initial Recipe design - WIP

---
+++ ChefRisingStar/ViewModels/RecipesViewModel.cs
@@ -0,0 +1,98 @@
+﻿using ChefRisingStar.Models;
+using ChefRisingStar.Services;
+using ChefRisingStar.Views;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ChefRisingStar.ViewModels
+{
+    public class RecipesViewModel : BaseViewModel
+    {
+        private Recipe _selectedItem;
+        public ObservableCollection<Recipe> recipesList { get; }
+        public Command LoadItemsCommand { get; }
+        public Command AddItemCommand { get; }
+        public Command<Achievement> ItemTapped { get; }
+        public IDataStore<Recipe, int> RecipeDataStore => DependencyService.Get<IDataStore<Recipe, int>>();
+
+        public RecipesViewModel()
+        {
+            
+            Title = "RecipesList";
+            recipesList = new ObservableCollection<Recipe>();
+            LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
+
+        }
+
+        async Task ExecuteLoadItemsCommand()
+        {
+            IsBusy = true;
+
+            try
+            {
+                recipesList.Clear();
+                var items = await RecipeDataStore.GetItemsAsync(true);
+                foreach (var item in items)
+                {
+                    recipesList.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        public void OnAppearing()
+        {
+            IsBusy = true;
+            SelectedItem = null;
+
+            //recipesList.ItemSource = null;
+            //recipesList.ItemSource = App.AllRecipes;
+        }
+
+        public Recipe SelectedItem
+        {
+            get => _selectedItem;
+            set
+            {
+                SetProperty(ref _selectedItem, value);
+                OnItemSelected(value);
+            }
+        }
+
+        async void OnItemSelected(Recipe recipe)
+        {
+            if (recipe == null)
+                return;
+
+            // This will push the ItemDetailPage onto the navigation stack
+            //TODO: This detail
+            await Shell.Current.GoToAsync($"{nameof(ItemDetailPage)}?{nameof(ItemDetailViewModel.ItemId)}={recipe.Id}");
+        }
+
+        /*
+        async void Add_Clicked(object sender, System.EventArgs e)
+        {
+            var editPage = new RecipeEditPage();
+
+            var editNavPage = new NavigationPage(editPage)
+            {
+                BarBackgroundColor = Color.FromHex("#01487E"),
+                BarTextColor = Color.White
+            };
+
+            await Navigation.PushModalAsync(editNavPage);
+        }
+        */
+    }
+}
+
---
+++ ChefRisingStar/Views/RecipesListPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using ChefRisingStar.Models;
+using ChefRisingStar.ViewModels;
+using ChefRisingStar.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ChefRisingStar.Views
+{
+    public partial class RecipesListPage : ContentPage
+    {
+        RecipesViewModel _viewModel;
+
+        public RecipesListPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new RecipesViewModel();
+
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+
+    }
+}
