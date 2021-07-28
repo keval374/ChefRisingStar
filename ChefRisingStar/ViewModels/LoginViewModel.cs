d21911a3ae90a258ce44ca134f3a824c1c3a3f54
Keval Pandit <kevalpandit@Ajays-MBP.fios-router.home>
2021-07-28 14:50:04 -0400

    backend code for login

--- ChefRisingStar/ViewModels/LoginViewModel.cs
+++ ChefRisingStar/ViewModels/LoginViewModel.cs
@@ -1,21 +1,57 @@
 ﻿using ChefRisingStar.Views;
 using Xamarin.Forms;
+using System;
 
 namespace ChefRisingStar.ViewModels
 {
     public class LoginViewModel : BaseViewModel
     {
+        private string email;
+        private string password;
+
+        public Action DisplayInvalidLoginPrompt;
         public Command LoginCommand { get; }
+
+        public string Email
+        {
+            get => email;
+            set
+            {
+                if (email == value)
+                    return;
+
+                email = value;
+                OnPropertyChanged("Email");
+            }
+        }
+
+        public string Password
+        {
+            get => password;
+            set
+            {
+                if (password == value)
+                    return;
+
+                password = value;
+                OnPropertyChanged("Password");
+            }
+            
+        }
 
         public LoginViewModel()
         {
             LoginCommand = new Command(OnLoginClicked);
         }
 
-        private async void OnLoginClicked(object obj)
+        private async void OnLoginClicked()
         {
+            if (email != "keval374@gmail.com" || password != "password")
+            {
+                DisplayInvalidLoginPrompt();
+            }
             // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
-            await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+            await Shell.Current.GoToAsync($"//{nameof(RecipesListPage)}");
         }
     }
 }
