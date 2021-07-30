ca179778739bb89e82c6b0f485664cfef04a87fc
Keval Pandit <kevalpandit@Ajays-MBP.fios-router.home>
2021-07-29 22:01:24 -0400

    corrected login logic

---
+++ ChefRisingStar/Views/KevalKiRecipeAddPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ChefRisingStar.Views
+{	
+	public partial class KevalKiRecipeAddPage : ContentPage
+	{	
+		public KevalKiRecipeAddPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
